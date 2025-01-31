df4d604dc84e8a43c5659f0bac46449d9bf9e76f
Okpara202 <okparafavour202@gmail.com>
2025-01-31 15:02:59 +0100

    bug fix

--- js/index.js
+++ js/index.js
@@ -1,6 +1,7 @@
 const btn = document.getElementById("nav-btn");
 const ul = document.getElementById("ul");
 const x = document.getElementById("x");
+const toTopBtn = document.getElementById("back-to-top");
 
 btn.onclick = () => {
   ul.classList.toggle("show");
@@ -12,4 +13,16 @@ x.onclick = () => {
   ul.classList.toggle("show");
   x.style.display = "none";
   btn.style.display = "block";
+};
+
+window.onscroll = () => {
+  if (window.scrollY > 300) {
+    toTopBtn.style.display = "block";
+  } else {
+    toTopBtn.style.display = "none";
+  }
+};
+
+toTopBtn.onclick = () => {
+  window.scrollTo({ top: 0, behavior: "smooth" });
 };
